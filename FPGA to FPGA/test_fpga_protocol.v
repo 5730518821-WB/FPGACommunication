09b103d307269bb504a9e396bb0931e40d8dc48b
Tara Tammanutam <pouy1942@hotmail.com>
2016-05-21 22:23:59 +0700

    chang order of test all block

--- FPGA to FPGA/test_fpga_protocol.v
+++ FPGA to FPGA/test_fpga_protocol.v
@@ -23,142 +23,139 @@ clock = 0;
 sent_data = 0;
 start = 0;
 
-#100 start = 1 ;
-#150 ;
 
-#100 send_data = 8'b00000001;
-while (busy) #100;
-
+// ------------- start ------------
+#100 sent_data = 8'b00000001;
+#100 start = 1;
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 1 round---------
+
+#100 sent_data = 8'b0000_0010;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0000_0010;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 2 round---------
+
+
+
+#100 sent_data = 8'b0000_0100;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0000_0100;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 3 round---------
+
+
+
+#100 sent_data = 8'b0000_1000;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0000_1000;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 4 round---------
+
+
+
+#100 sent_data = 8'b0001_0000;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0001_0000;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 5 round---------
+
+
+
+#100 sent_data = 8'b0010_0000;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0010_0000;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 6 round---------
+
+
+
+#100 sent_data = 8'b0100_0000;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0100_0000;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 7 round---------
+
+
+
+#100 sent_data = 8'b1000_0000;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b1000_0000;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 8 round---------
 // ------- end test 1bits *8 shift ------------
+
+
+
+#100 sent_data = 8'b0001_0001;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0001_0001;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 9 round---------
+
+
+
+#100 sent_data = 8'b0010_0010;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0010_0010;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 10 round---------
+
+
+
+#100 sent_data = 8'b0100_0100;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0100_0100;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 11 round---------
+
+
+
+#100 sent_data = 8'b1000_1000;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b1000_1000;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 12 round---------
 // ------- end test 2bits *4 ------------
+
+
+
+#100 sent_data = 8'b0101_0101;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b0101_0101;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 13 round---------
+
+
+
+#100 sent_data = 8'b1010_1010;
 #100 start = 1;
-#150 ;
-
-#100 send_data = 8'b1010_1010;
-while (busy) #100;
-
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 14 round---------
 // -------- end 4 bit shift *2 -----------
-#100 start = 1;
+
 #150 ;
 
-#100 send_data = 8'b1111_1111;
-while (busy) #100;
-
+#100 sent_data = 8'b1111_1111;
+#100 start = 1;
 #100 start = 0;
+while (busy) #100;
 #100 ;
 // ------- end 15 round---------
 
