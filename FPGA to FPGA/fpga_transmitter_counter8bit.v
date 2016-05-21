c9647f20f1012bde69fd1b70a47d05a44bbb2683
ZanooKer <snooker49513@gmail.com>
2016-05-22 00:21:50 +0700

    fix countNumber in fpga transmitter

--- FPGA to FPGA/fpga_transmitter_counter8bit.v
+++ FPGA to FPGA/fpga_transmitter_counter8bit.v
@@ -2,8 +2,8 @@ module counter8(count8,increment,clk,resetCounter);
 input increment,clk,resetCounter;
 output count8;
 
-reg[15:0] countNum;
-counter c0(countNum,15'b0,1'b0,increment,clk,resetCounter);
-assign count8 = (countNum == 8);
+wire[15:0] countNum;
+counter c0(countNum,16'b0,1'b0,increment,clk,resetCounter);
+assign count8 = (countNum == 7);
 
 endmodule
