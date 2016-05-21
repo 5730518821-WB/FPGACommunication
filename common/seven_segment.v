fd16a29e2400ca9d8fff05044f9628114ecf2321
Tara Tammanutam <pouy1942@hotmail.com>
2016-05-21 21:19:28 +0700

    change  always@(number)  to always@(*)

--- common/seven_segment.v
+++ common/seven_segment.v
@@ -3,7 +3,7 @@ module seven_segment(
     input [3:0] number       //  input is number
     );
 
-always @(number) begin          // show number from case
+always @(*) begin          // show number from case
     case (number)
     4'h0: result = 8'b11111100;
     4'h1: result = 8'b01100000;
@@ -11,6 +11,7 @@ always @(number) begin          // show number from case
     4'h3: result = 8'b11110010;
     4'h4: result = 8'b01100110;
     4'h5: result = 8'b10110110;
+    4'h6: result = 8'b10111110;
     4'h7: result = 8'b11100000;
     4'h8: result = 8'b11111110;
     4'h9: result = 8'b11110110;
