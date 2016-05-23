ccffd865fb1212a0da907a0af9e2486ac8fce205
ZanooKer <snooker49513@gmail.com>
2016-05-24 02:31:51 +0700

    fix sdc Writer Counter

--- FPGA to SDC/sdc_writer_counterData.v
+++ FPGA to SDC/sdc_writer_counterData.v
@@ -6,6 +6,7 @@ wire[15:0] countNum;
 counter c0(countNum,16'b0,1'b0,count,clk,resetCounter);
 
 assign shift = count;
+assign load = (startCount | bytes);
 assign bytes = (countNum[2:0]==3'b111);
 assign block = (countNum[15:0]==(16'b0001_0000_0000_1000));
 assign endCRC = (countNum[15:0]==(16'b0001_0000_0001_1000));
